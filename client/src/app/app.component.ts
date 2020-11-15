882ddc173a812a8c1696be4696b34ca077956894
tarek jawad <tarekjawadbaba@gmail.com>
2020-11-15 06:12:42 +0300

    lesson4

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Magma Sözlük';
+  users: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      (respones) => {
+        this.users = respones;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
