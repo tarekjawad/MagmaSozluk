2e041c81d4e665685b6d621a32829fc116331c9a
tarek jawad <tarekjawadbaba@gmail.com>
2020-11-17 19:08:53 +0300

    endofrouting

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
-
 
 @Component({
   selector: 'app-nav',
@@ -12,20 +13,28 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login() {
-    this.accountService.login(this.model).subscribe(response =>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    });
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        this.router.navigateByUrl('/members');
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error(error.error);
+      }
+    );
   }
 
-  logout(){
+  logout() {
     this.accountService.logut();
+    this.router.navigateByUrl('/');
   }
 }
